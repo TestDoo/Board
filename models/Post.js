b14ce1c0d019c4e65c5ecdb73a7f9170b50e3295
testDoo <guhyvv1992@gmail.com>
2021-04-21 23:34:50 +0900

    first front-end

--- models/Post.js
+++ models/Post.js
@@ -9,8 +9,8 @@ var postSchema = mongoose.Schema({
     // 2 : default 항목으로 기본값을 지정한다.
     // 함수명을 넣으면 해당 함수의 return이 기본값이 됨
     // Date.now는 현재 시간을 리턴하는 함수이다
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date },
+    createdAt: { type: Date, default: Date.now }, // 2
+    updatedAt: { type: Date },
 });
 
 // model & export
